dd72018c07b75ac6579a0405d280ee72f42158c3
Marc Brooks <markwithasee@gmail.com>
2014-05-12 15:38:54 -0400

    initial commit

---
+++ jquery.truncate.js
@@ -0,0 +1,64 @@
+/**
+ * jquery.truncate.js v0.1.0
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Marc D. Brooks
+ * https://github.com/markwithasee/truncate
+ */
+
+(function($){
+	
+$.fn.truncate = function(limit) {
+    
+  var textLimit = (typeof limit === 'undefined') ? 20 : limit;
+  var thisClass = $(this).attr('class');
+		
+  function shorten (text, limit, prepend, append) {
+    if (typeof text !== 'string')
+        return '';
+    var parts = text.split(' ');
+    if (parts.length > limit) {
+        for (var i = parts.length - 1; i > -1; --i) {
+            if (i+1 > limit) {
+                parts.length = i;
+            }
+        }
+
+        parts.splice(0, 0, prepend);
+        
+        var lastWord = parts[parts.length-1];
+        if( lastWord.indexOf('.') != -1 || lastWord.indexOf('!') != -1 || lastWord.indexOf('?') != -1){
+		    parts.push(append);
+		}else{
+			parts.push(" ... " + append);
+		}
+
+    }
+    return parts.join(' ');
+  }
+  
+  this.each(function(){
+  
+    var thisObj = $(this),
+        fullText = thisObj.text(),
+        moreLink = "<a href='#' class='more'>More &raquo;</a>",
+        lessLink = "<a href='#' class='less'>&laquo; Less</a>",
+        prepend = '<div class="truncated">',
+        append = moreLink + '</div><div class="expanded">' + fullText + ' ' + lessLink + '</div>',
+        shortText = shorten(fullText, textLimit, prepend, append);
+  
+    thisObj.html(shortText);
+  
+  });
+  
+  $('.'+thisClass).find('.more, .less').on('click',function(e){
+    e.preventDefault();
+    var item = $(this).parents('.'+thisClass);
+    item.find('.truncated, .expanded').toggle();
+  });
+
+ };
+ 
+})(jQuery);
